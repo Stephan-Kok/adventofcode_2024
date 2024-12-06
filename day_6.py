24454420160dbfddb6231ea12d9effcd7dacce4f
Stephan Kok <kok.stephan@kmsgroep.com>
2024-12-06 22:04:51 +0100

    day 6

--- day_6.py
+++ day_6.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import multiprocessing
 from enum import Enum
 
 time_start = datetime.now()
@@ -33,51 +34,36 @@ class Obstructions(Enum):
     OBJECT = 1
     FREE = 2
 
-
-object_count = {} # Loop counter hack
-def check_obstructions(pos_x: int, pos_y: int):
-    if pos_x < 0 or pos_x > width - 1 or pos_y < 0 or pos_y > height - 1:
-        return Obstructions.FREE
-    if matrix[pos_y][pos_x] == ".":
-        return Obstructions.OPEN
-    if matrix[pos_y][pos_x] == "#":
-        # Loop counter hack for part2
-        index = "X:" + str(pos_x) + "Y:" + str(pos_y)
-        object_count[index] = object_count.get(index, 0) + 1
-        if object_count[index] > 5:
-            raise ValueError("PossibleLoopEncountered")
-        return Obstructions.OBJECT
-    if matrix[pos_y][pos_x] == "X":
-        return Obstructions.OPEN
-    return None
-
-
 class Guard:
     direction = Direction.UP
     x: int
     y: int
+    matrix: [[str]]
+    object_count: dict # Loop counter hack
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, matrix):
         self.x = x
         self.y = y
+        self.matrix = matrix
+        self.object_count = {}
 
     def walk(self):
         (x, y) = self.next_move()
-        obstruction = check_obstructions(x, y)
+        obstruction = self.check_obstructions(x, y)
         while obstruction != Obstructions.FREE:
             while obstruction == Obstructions.OBJECT:
                 self.change_direction()
                 (x, y) = self.next_move()
-                obstruction = check_obstructions(x, y)
+                obstruction = self.check_obstructions(x, y)
             self.move(x, y)
             (x, y) = self.next_move()
-            obstruction = check_obstructions(x, y)
-        matrix[self.y][self.x] = "X"
+            obstruction = self.check_obstructions(x, y)
+        self.matrix[self.y][self.x] = "X"
         # print("Motherfucker free")
 
     def move(self, x, y):
-        matrix[self.y][self.x] = "X"
-        matrix[y][x] = "^"
+        self.matrix[self.y][self.x] = "X"
+        self.matrix[y][x] = "^"
         self.x = x
         self.y = y
 
@@ -95,10 +81,26 @@ class Guard:
     def change_direction(self):
         self.direction = Direction((self.direction.value + 1) % 4)
 
+    def check_obstructions(self, pos_x: int, pos_y: int):
+        if pos_x < 0 or pos_x > width - 1 or pos_y < 0 or pos_y > height - 1:
+            return Obstructions.FREE
+        if self.matrix[pos_y][pos_x] == ".":
+            return Obstructions.OPEN
+        if self.matrix[pos_y][pos_x] == "#":
+            # Loop counter hack for part2
+            index = "X:" + str(pos_x) + "Y:" + str(pos_y)
+            self.object_count[index] = self.object_count.get(index, 0) + 1
+            if self.object_count[index] > 5:
+                raise ValueError("PossibleLoopEncountered")
+            return Obstructions.OBJECT
+        if self.matrix[pos_y][pos_x] == "X":
+            return Obstructions.OPEN
+        return None
+
 # part1
-guard = Guard(start_x, start_y)
+guard = Guard(start_x, start_y, matrix)
 guard.walk()
-print(sum([line.count("X") for line in matrix]))
+print(sum([line.count("X") for line in guard.matrix]))
 
 # part2
 # This part is dependable on part1!
@@ -107,14 +109,10 @@ print(sum([line.count("X") for line in matrix]))
 walked_paths = []
 for i in range(height):
     for j in range(width):
-        if matrix[i][j] == "X":
+        if guard.matrix[i][j] == "X":
             walked_paths.append((j, i))
 
-total_loops = 0
-for mark_x, mark_y in walked_paths:
-    # Reset loop counter
-    object_count = {}
-
+def part2(mark_x, mark_y):
     # Reset matrix and place new object
     matrix = []
     for line in lines:
@@ -122,19 +120,34 @@ for mark_x, mark_y in walked_paths:
     matrix[mark_y][mark_x] = "#"
 
     # Escape and log if loop found
-    guard = Guard(start_x, start_y)
+    guard = Guard(start_x, start_y, matrix)
     try:
         guard.walk()
     except ValueError as e:
         if e.args[0] == "PossibleLoopEncountered":
-            total_loops += 1
+            return True
         else:
             print(e)
-print("Total loops", total_loops)
+    return False
 
-# for row in matrix:
-#     print("".join(map(str, row)))
+def collect_result(result):
+    if (result):
+        results.append(result)
 
+if __name__ == '__main__':
+    with multiprocessing.Manager() as manager:
+        # Shared list to collect results
+        results = manager.list()
+
+        with multiprocessing.Pool(processes=8) as pool:
+            for mark_x, mark_y in walked_paths:
+                pool.apply_async(part2, args=(mark_x, mark_y), callback=collect_result)
+
+            pool.close()
+            pool.join()
+
+        # Summing all results
+        print("Total loops:", len(results))
 
 time_end = datetime.now()
 print("Total time: ", time_end - time_start)
