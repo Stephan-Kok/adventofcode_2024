dd23f20cb9b3568c117d406df4e26f909e986f97
Stephan Kok <kok.stephan@kmsgroep.com>
2024-12-07 12:15:45 +0100

    day 7

--- day_7.py
+++ day_7.py
@@ -9,6 +9,7 @@ with open('day_7.input', 'r') as file:
 
 data = []
 number_pattern = r"^[0-9]+"
+
 for line in lines:
     result = re.match(number_pattern, line).group(0)
     total = int(result)
@@ -18,39 +19,23 @@ for line in lines:
 def get_combinations(array_a, b, operators: [operator]):
     return [fun(a,b) for fun in operators for a in array_a]
 
-
-result = 0
-operators = [operator.add, operator.mul]
-for total, numbers in data:
-    problem_space = len(numbers) ** len(operators)
-
-    prev = [numbers[0]]
-    for i in range(1, len(numbers)):
-        next = numbers[i]
-        prev = get_combinations(prev, next, operators)
-
-    if total in prev:
-        result += total
-
-print("Part1", result)
-
 def concatenation(a, b):
     return int(str(a)+str(b))
 
-result = 0
-operators = [operator.add, operator.mul, concatenation]
-for total, numbers in data:
-    problem_space = len(numbers) ** len(operators)
+def bridge_repair(operators):
+    result = 0
+    for total, numbers in data:
+        prev = [numbers[0]]
+        for i in range(1, len(numbers)):
+            next = numbers[i]
+            prev = get_combinations(prev, next, operators)
 
-    prev = [numbers[0]]
-    for i in range(1, len(numbers)):
-        next = numbers[i]
-        prev = get_combinations(prev, next, operators)
+        if total in prev:
+            result += total
+    return result
 
-    if total in prev:
-        result += total
-
-print("part2", result)
+print("Part1", bridge_repair([operator.add, operator.mul]))
+print("Part2", bridge_repair([operator.add, operator.mul, concatenation]))
 
 time_end = datetime.now()
 print("Total time: ", time_end - time_start)
